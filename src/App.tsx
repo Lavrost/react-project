f90c1c1c0b2d61c41ce92ec4e94c2add509cb783
Rostislav Lavrinenko <lav.rost@yandex.ru>
2023-10-16 17:59:58 +0300

    Добавил css modules

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Counter } from "./components/Counter/Counter";
+import "./index.scss";
+
+
+const App = () => {
+    return (
+        <div className="app">
+            Hello, world!
+            <Counter />
+        </div>
+    )
+}
+
+export default App;
