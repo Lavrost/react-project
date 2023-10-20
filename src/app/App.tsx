e221cf735f371f3ddc8a6727819971536d1ee7a0
Rostislav Lavrinenko <lav.rost@yandex.ru>
2023-10-20 14:38:35 +0300

    Начал внедрять архитектуру Feature-Sliced

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
 import { Route, Routes, Link } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import "./styles/index.scss";
-import { useTheme } from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const App = () => {
@@ -16,8 +16,8 @@ const App = () => {
             <Link to={'/about'}>О нас</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />}/>
                 </Routes>
             </Suspense>
         </div>
