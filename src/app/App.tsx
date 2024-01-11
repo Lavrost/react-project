333fe8ed61b54ae57d9f3e4313b60372d1586efe
Rostislav Lavrinenko <lav.rost@yandex.ru>
2024-01-11 18:16:23 +0300

    Добавил отдельный компонент для переключениятемы

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,14 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher/ui/ThemeSwitcher";
 
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme()
+    const { theme } = useTheme()
     return (
         <div className={classNames('app', {hovered: true, selected: false}, [theme])}>
             <Navbar/>
-            <button onClick={toggleTheme}>Сменить тему</button>
             <AppRouter/>
         </div>
     )
